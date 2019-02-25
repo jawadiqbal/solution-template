90c0c4c5f862f352b0afdc8bfbf33d33ba69a821
Jawad Iqbal <jawad.iqbal92@gmail.com>
2019-02-25 23:42:23 +0600

    solution implemented and fixed sort approach

--- src/main/java/com/tigerit/exam/Solution.java
+++ src/main/java/com/tigerit/exam/Solution.java
@@ -1,10 +1,7 @@
 package com.tigerit.exam;
 
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
 
 import static com.tigerit.exam.IO.*;
 
@@ -17,18 +14,8 @@ import static com.tigerit.exam.IO.*;
 public class Solution implements Runnable {
     @Override
     public void run() {
-        // your application entry point
-
-        // sample input process
-        // String string = readLine();
-
-        // Integer integer = readLineAsInteger();
-
-        // sample output process
-        // printLine(string);
-        // printLine(integer);
-
         Integer T = readLineAsInteger();
+        int caseNo = 1;
         while(T > 0) {
             T = T - 1;
 
@@ -59,17 +46,136 @@ public class Solution implements Runnable {
                     table.data.add(rowDataList);
                 }
 
-                // table.printTable();
                 tableList.add(table);
             }
 
-            // Integer nQ = readLineAsInteger();
-            // while(nQ > 0) {
-            //     nQ = nQ - 1;
-            //
-            //
-            // }
+            List<Query> queryList = new ArrayList<>();
+            Integer nQ = readLineAsInteger();
+            while(nQ > 0) {
+                nQ = nQ - 1;
+
+                Query query = new Query();
+
+                String line1 = readLine();
+                query.fields = new ArrayList<>();
+                List<String> line1List = Arrays.asList(line1.split(" "));
+                if(line1List.get(1).equals("*")) {
+                    query.fields.add("*");
+                } else {
+                    for(String s: line1List) {
+                        if(!s.equals("SELECT")) {
+                            query.fields.add(s.replace(",", ""));
+                        }
+                    }
+                }
+
+                String line2 = readLine();
+                List<String> line2List = Arrays.asList(line2.split(" "));
+                for(Table t: tableList) {
+                    if(t.name.equals(line2List.get(1))) {
+                        query.tableLeft = t;
+                        break;
+                    }
+                }
+                if(line2List.size() == 3) {
+                    query.shortNameForLeft = line2List.get(2);
+                } else {
+                    query.shortNameForLeft = null;
+                }
+
+                String line3 = readLine();
+                List<String> line3List = Arrays.asList(line3.split(" "));
+                for(Table t: tableList) {
+                    if(t.name.equals(line3List.get(1))) {
+                        query.tableRight = t;
+                        break;
+                    }
+                }
+                if(line3List.size() == 3) {
+                    query.shortNameForRight = line3List.get(2);
+                } else {
+                    query.shortNameForRight = null;
+                }
+
+                String line4 = readLine();
+                List<String> line4List = Arrays.asList(line4.split(" "));
+                String leftCondition = line4List.get(1);
+                String rightCondition = line4List.get(3);
+
+                List<String> leftList = Arrays.asList(leftCondition.split("\\."));
+                if(query.shortNameForLeft != null && query.shortNameForLeft.equals(leftList.get(0)) || query.tableLeft.name.equals(leftList.get(0))) {
+                    query.leftColumnForJoin = leftList.get(1);
+                } else if(query.shortNameForLeft != null && query.shortNameForRight.equals(leftList.get(0)) || query.tableRight.name.equals(leftList.get(0))) {
+                    query.rightColumnForJoin = leftList.get(1);
+                }
+
+                List<String> rightList = Arrays.asList(rightCondition.split("\\."));
+                if(query.shortNameForLeft != null && query.shortNameForLeft.equals(rightList.get(0)) || query.tableLeft.name.equals(rightList.get(0))) {
+                    query.leftColumnForJoin = rightList.get(1);
+                } else if(query.shortNameForLeft != null && query.shortNameForRight.equals(rightList.get(0)) || query.tableRight.name.equals(rightList.get(0))) {
+                    query.rightColumnForJoin = rightList.get(1);
+                }
+
+                queryList.add(query);
+            }
+
+            printLine("Test: " + caseNo);
+            for(Query q: queryList) {
+                Table t = q.generateResult();
+                t.printTable();
+            }
         }
+    }
+}
+
+class Query {
+    List<String> fields;
+    Table tableLeft;
+    String shortNameForLeft;
+    Table tableRight;
+    String shortNameForRight;
+    String leftColumnForJoin;
+    String rightColumnForJoin;
+
+    Query() {
+    }
+
+    Table generateResult() {
+        Table table = new Table();
+
+        table.columns = new ArrayList<>();
+        if(this.fields.size() == 1 && this.fields.get(0).equals("*")) {
+            table.columns.addAll(tableLeft.columns);
+            table.columns.addAll(tableRight.columns);
+        } else {
+            for(String s: this.fields) {
+                table.columns.add(Arrays.asList(s.split("\\.")).get(1));
+            }
+        }
+
+        int leftColumnIndex = tableLeft.columns.indexOf(this.leftColumnForJoin);
+        int rightColumnIndex = tableRight.columns.indexOf(this.rightColumnForJoin);
+
+        table.data = new ArrayList<>();
+        for(List<Integer> leftRow: tableLeft.data) {
+            for(List<Integer> rightRow: tableRight.data) {
+                if(leftRow.get(leftColumnIndex) == rightRow.get(rightColumnIndex)) {
+                    List<Integer> row = new ArrayList<>();
+                    for(String column: table.columns) {
+                        int idx = tableLeft.columns.indexOf(column);
+                        if(idx != -1) {
+                            row.add(leftRow.get(idx));
+                        } else {
+                            idx = tableRight.columns.indexOf(column);
+                            row.add(rightRow.get(idx));
+                        }
+                    }
+                    table.data.add(row);
+                }
+            }
+        }
+
+        return table;
     }
 }
 
@@ -83,27 +189,7 @@ class Table {
     Table() {
     }
 
-    Table(String name, Integer numOfColumns, Integer numOfRecords, List<String> columns, List<List<Integer>> data) {
-        this.name = name;
-        this.numOfColumns = numOfColumns;
-        this.numOfRecords = numOfRecords;
-        this.columns = columns;
-        this.data = data;
-    }
-
-    @Override
-    public String toString() {
-        final StringBuilder sb = new StringBuilder("Table{");
-        sb.append("name='").append(name).append('\'');
-        sb.append(", numOfColumns=").append(numOfColumns);
-        sb.append(", numOfRecords=").append(numOfRecords);
-        sb.append(", columns=").append(columns);
-        sb.append(", data=").append(data);
-        sb.append('}');
-        return sb.toString();
-    }
-
-    public void printTable() {
+    void printTable() {
         StringBuilder printString = new StringBuilder();
         boolean first = true;
         for(String s: this.columns) {
@@ -115,7 +201,18 @@ class Table {
             }
         }
 
-        for(List<Integer> row: this.data) {
+        List<List<Integer>> printData = this.data;
+        printData.sort((a1, a2) -> {
+            for (int i = 0; i < Math.min(a1.size(), a2.size()); i++) {
+                int ret = a1.get(i).compareTo(a2.get(i));
+                if (ret != 0) {
+                    return ret;
+                }
+            }
+            return Integer.compare(a1.size(), a1.size());
+        });
+
+        for(List<Integer> row: printData) {
             printString.append("\n");
             first = true;
             for(Integer i: row) {
